1708db9f356dbc2df6d6873834e3d47835f3c712
nisekoi <nisekoi@qq.com>
2024-08-13 19:47:52 +0800

    lodash第二次提交

--- lodash/xzlsq-lodash.js
+++ lodash/xzlsq-lodash.js
@@ -73,5 +73,71 @@ var xzlsq = {
         }
 
         return -1
+    },
+
+    flatten: function flatten(array) {
+        var res = []
+        for (var i = 0; i < array.length; i++) {
+            if (Array.isArray(array[i])) {
+                for (var j = 0; j < array[i].length; j++) {
+                    res.push(array[i][j])
+                }
+            } else {
+                res.push(array[i])
+            }
+        }
+
+        return res
+    },
+
+    flattenDeep: function flattenDeep(array, res = []) {
+        for (var i = 0; i < array.length; i++) {
+            if (Array.isArray(array[i])) {
+                flattenDeep(array[i],res)
+            } else {
+                res.push(array[i])
+            }
+        }
+
+        return res
+    },
+
+    flattenDepth: function flattenDepth(array, depth = 1, res = []) {
+        if (depth < 0) {
+            return res.push(array)
+        }
+
+        for (var i = 0; i < array.length; i++) {
+            if (Array.isArray(array[i])) {
+                flattenDepth(array[i], depth - 1, res)
+            } else {
+                res.push(array[i])
+            }
+        }
+
+        return res
+    },
+
+    fromPairs: function fromPairs(pairs) {
+        var res = {}
+
+        for (var i = 0; i < pairs.length; i++) {
+            for (var j = 0; j < pairs[i].length; j++) {
+                res[pairs[i][j]] = pairs[i][++j]
+            }
+        }
+
+        return res
+    },
+
+    toPairs: function toPairs(object) {
+        var res = []
+        for (var key in object) {
+            if (key in object.__proto__) {
+                continue
+            }
+            res.push([key,object[key]])
+        }
+        return res
     }
 }
